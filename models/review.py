21f43a9c5f819ceb231e94e2bab909aa69072fff
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-08-13 10:52:40 -0500

    add foranea key to review

--- models/review.py
+++ models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Review module for the HBNB project."""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
@@ -9,5 +9,5 @@ class Review(BaseModel, Base):
 
     __tablename__ = 'reviews'
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), nullable=False)
-    user_id = Column(String(60), nullable=False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
