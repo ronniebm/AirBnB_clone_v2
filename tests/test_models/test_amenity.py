ee4a6072f7f2fcd0b8676bad497b491a2e24a15e
ronniebm <ronnie.coding@gmail.com>
2020-08-13 12:44:52 -0500

    unittest for test_amenity.py updated

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-"""Amenity class"""
+"""Module for test Amenity class"""
+import unittest
+import json
 import pep8
+import datetime
 
-from tests.test_models.test_base_model import test_basemodel
 from models.amenity import Amenity
 from models.base_model import BaseModel
 
 
-class test_Amenity(test_basemodel):
-    """Test Amenity"""
-
-    def __init__(self, *args, **kwargs):
-        """init"""
-        super().__init__(*args, **kwargs)
-        self.name = "Amenity"
-        self.value = Amenity
-
-    def test_name2(self):
-        """test_name2 """
-        new = self.value()
-        self.assertEqual(type(new.name), str)
-
+class TestAmenity(unittest.TestCase):
+    """Test State class implementation"""
     def test_doc_module(self):
         """Module documentation"""
         doc = Amenity.__doc__
@@ -44,14 +34,6 @@ class test_Amenity(test_basemodel):
         """Constructor documentation"""
         doc = Amenity.__init__.__doc__
         self.assertGreater(len(doc), 1)
-
-    def test_class(self):
-        """Validate the types of the attributes an class"""
-        with self.subTest(msg='Inheritance'):
-            self.assertTrue(issubclass(Amenity, BaseModel))
-
-        with self.subTest(msg='Attributes'):
-            self.assertIsInstance(Amenity.name, str)
 
 
 if __name__ == '__main__':
