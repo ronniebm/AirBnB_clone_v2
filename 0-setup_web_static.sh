8f504d99bca8586668b6870d75c1f93b51cef271
ronniebm <ronnie.coding@gmail.com>
2020-08-17 14:38:43 -0500

    fix 06

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -33,7 +33,7 @@ echo '<html>
 # ------------------------------
 # 3. re-creating soft links.
 
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
 
 # ------------------------------
 # 4. give ownership of '/data' folder to ubuntu user.
@@ -43,9 +43,10 @@ sudo chown -hR ubuntu:ubuntu /data/
 # ------------------------------
 # 5. Update Nginx config. to serve content of
 #    /data/web_static/current/ to hbnb_static.
+#TEXT="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
+#sed -i "/server_name/a $TEXT" /etc/nginx/sites-available/default
 
-TEXT="\\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
-sed -i "/server_name/a $TEXT" /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # ------------------------------
 # 6. Restart Nginx service.
