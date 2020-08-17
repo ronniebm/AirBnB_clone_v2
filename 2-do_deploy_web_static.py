156e0095a60feda8ec495a5b2579cee755c207c3
ronniebm <ronnie.coding@gmail.com>
2020-08-17 16:37:55 -0500

    fix try#01

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,12 +4,12 @@ A Fabric script (based on the file 1-pack_web_static.py) that distributes
 an archive to my web servers, using the function do_deploy.
 """
 import os.path
-from fabric.api import put, run
+from fabric.api import *
 from fabric.operations import run, put, sudo
 
 """my server's ip addresses"""
 env.hosts = ['35.243.144.124', '3.91.55.208']
-
+env.user = "ubuntu"
 
 def do_deploy(archive_path):
     """do_deploy function."""
