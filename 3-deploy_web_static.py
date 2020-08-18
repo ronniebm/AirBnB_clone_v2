b2b6623502f5c8c990cbc8b720c487c54ef7af6d
ronniebm <ronnie.coding@gmail.com>
2020-08-17 20:49:28 -0500

    try#06

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -37,14 +37,14 @@ def do_deploy(archive_path):
         new_folder = ("/data/web_static/releases/" + file_name.split(".")[0])
 
         put(archive_path, "/tmp")
-        run("sudo mkdir -p {}".format(new_folder))
-        run("sudo tar -xzf /tmp/{} -C {}".
+        run("mkdir -p {}".format(new_folder))
+        run("tar -xzf /tmp/{} -C {}".
             format(file_name, new_folder))
-        run("sudo rm /tmp/{}".format(file_name))
-        run("sudo mv {}/web_static/* {}/".format(new_folder, new_folder))
-        run("sudo rm -rf {}/web_static".format(new_folder))
-        run('sudo rm -rf /data/web_static/current')
-        run("sudo ln -s {} /data/web_static/current".format(new_folder))
+        run("rm /tmp/{}".format(file_name))
+        run("mv {}/web_static/* {}/".format(new_folder, new_folder))
+        run("rm -rf {}/web_static".format(new_folder))
+        run('rm -rf /data/web_static/current')
+        run("ln -s {} /data/web_static/current".format(new_folder))
         return True
 
     except:
