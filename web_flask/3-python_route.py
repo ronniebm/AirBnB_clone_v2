48ea7c68c1755e5cb9ea0e653f3266e21de107cf
ronniebm <ronnie.coding@gmail.com>
2020-09-01 13:08:27 -0500

    trying task#3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+starts a Flask web app.
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello():
+    """a script that starts a Flask
+    web application"""
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def route1():
+    """a script that starts a Flask
+    web application"""
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def route2(text):
+    """It returns text passed by url"""
+    return "C " + text.replace('_', ' ')
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def route3(text):
+    """It returns text passed by url"""
+    return "Python " + text.replace('_', ' ')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port="5000")
