21ab4fe9fc5388555d85ef626526f7211cb423f6
ronniebm <ronnie.coding@gmail.com>
2020-09-02 16:17:48 -0500

    trying task#10

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+starts a Flask web app.
+"""
+from models import storage
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """Remove the current SQLAlchemy session."""
+    storage.close()
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def route1():
+    """Displays an HTML with a list of all States.
+    """
+    url = "10-hbnb_filters.html"
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template(url, state=states,
+                        amenities=amenities)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
